47915a29f38f5b31bc1a30913b46188829c7dfda
BohdanPastuschak <bohdanpastuschak@gmail.com>
2020-05-19 13:12:52 +0300

    Create Naive.cpp

---
+++ Experiments/Experiment1/Naive.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T, typename R>
+struct parabola
+{
+	T a, b, c;
+	parabola(){}
+	parabola(T _a, T _b, T _c): a(_a), b(_b), c(_c) {}
+	
+	R query(int x)
+	{
+		return a * (R) x * x + b * (R) x + c;
+	}
+};
+
+typedef parabola<int, long long> Function;
+
+int main()
+{
+	vector<Function> S;
+	int q;
+	scanf("%d", &q);
+	long long result = 0;
+	while(q--)
+	{
+		int type;
+		scanf("%d", &type);
+		if (type == 1)
+		{
+			int a, b, c;
+			scanf("%d %d %d", &a, &b, &c);
+			S.push_back(Function(a, b, c));
+		}
+		else
+		{
+			int x;
+			scanf("%d", &x);
+			long long minimum = LLONG_MAX;
+			for(auto i: S)
+				minimum = min(minimum, i.query(x));
+			
+			result += minimum;
+			//printf("%lld\n", minimum);
+		}
+	}
+	
+	cerr << result << endl;
+	cout << clock() / (double) CLOCKS_PER_SEC << endl;
+	return 0;
+}
