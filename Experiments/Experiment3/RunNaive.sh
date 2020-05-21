cff931e495646c59cc3beb8436681b42f2ebe4ff
BohdanPastuschak <bohdanpastuschak@gmail.com>
2020-05-21 12:30:42 +0300

    Update RunNaive.sh

--- Experiments/Experiment3/RunNaive.sh
+++ Experiments/Experiment3/RunNaive.sh
@@ -1,6 +1,6 @@
 for((i = 1; i <= 10;++i)); do
 	echo $i
 	./GeneratorDoubles 10000 1073741824 1000000000 50 $i > in # need to pass T, C, M, P, seed
-	./NaiveDoubles < in > out
+	./NaiveDoubles < in >> out
 done 
 ./CalculateTime < out
