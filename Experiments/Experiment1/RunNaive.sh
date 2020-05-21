ac2617dd3e5cb78922a01d5a3a0006daf0fa2895
BohdanPastuschak <bohdanpastuschak@gmail.com>
2020-05-21 12:31:18 +0300

    Update RunNaive.sh

--- Experiments/Experiment1/RunNaive.sh
+++ Experiments/Experiment1/RunNaive.sh
@@ -1,6 +1,6 @@
 for((i = 1; i <= 100;++i)); do
 	echo $i
 	./Generator 10000 1048576 1000 50 $i > in # need to pass T, C, M, P, seed
-	./Naive < in > out
+	./Naive < in >> out
 done 
 ./CalculateTime < out
