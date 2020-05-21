38d610b0a97763bbd8081740af7be5fe21ea617f
BohdanPastuschak <bohdanpastuschak@gmail.com>
2020-05-21 12:33:55 +0300

    Add files via upload

---
+++ Experiments/Experiment4/GeneratorDoubles.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+inline int to_int(char* argv)
+{
+	int result = 0;
+	for(int i = 0; i < (int)strlen(argv); ++i)
+		result = 10 * result + argv[i] - '0';
+	
+	return result;
+}
+
+inline int random_int(int l, int r, mt19937& rng)
+{
+	return l + (rng() % (r - l + 1));
+}
+
+inline double random_double(int l, int r, mt19937& rng)
+{
+	double in01 = (double)(rng() % INT_MAX) / INT_MAX;
+	double in01_2 = (double)(rng() % INT_MAX) / INT_MAX;
+	return l + (r - l) * in01 + in01_2;
+}
+
+int main(int argc, char* argv[])
+{
+	assert(argc > 5);
+	
+	int T = to_int(argv[1]);//total number of queries
+	int C = to_int(argv[2]);//max coordinate
+	int M = to_int(argv[3]);
+	int P = to_int(argv[4]);//percentage of first type
+	mt19937 rng(to_int(argv[5]));//random device	
+	
+	int k = 1;
+	printf("%d\n", T);
+	for(int i = 0; i < T; ++i)
+	{
+		int type = i == 0 ? 1 : 1 + (random_int(0, 99, rng) < P);
+		printf("%d ", type);
+		if (type == 1)
+		{
+			double a = k;
+			double b = -k * (double) 2 * (k - 1);
+			double c = k * (double) (k - 1) * (k - 1);
+			k++;			
+			printf("%lf %lf %lf\n", a, b, c);
+		}
+		else
+		{
+			int x = random_int(-C, C, rng);
+			printf("%d\n", x);
+		}
+	}
+	
+	return 0;
+}
