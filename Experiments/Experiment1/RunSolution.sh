8c9bb74c323a8381bc0566634b05846d224dcdef
BohdanPastuschak <bohdanpastuschak@gmail.com>
2020-05-21 12:31:34 +0300

    Update RunSolution.sh

--- Experiments/Experiment1/RunSolution.sh
+++ Experiments/Experiment1/RunSolution.sh
@@ -1,6 +1,6 @@
 for((i = 1; i <= 100;++i)); do
 	echo $i
 	./Generator 10000 1048576 1000 50 $i > in # need to pass T, C, M, P, seed
-	./Solution < in > out
+	./Solution < in >> out
 done 
 ./CalculateTime < out
